2f11018f30e8ea30a430e9ef94162c1cdee1f624
Vera Ilina <vvegorova@gmail.com>
2019-10-02 21:30:52 +0200

    Added a separate class for weighted graph.

--- src/vvalgo/graph.py
+++ src/vvalgo/graph.py
@@ -1,45 +1,27 @@
 """Implementation of graph represented by an adjacency list."""
-from typing import Dict, List, Tuple, Optional
+from typing import Dict, List, Tuple, Optional, Union
 from pathlib import Path
+from operator import itemgetter
 
 from collections import defaultdict
 
 
 class Graph:
     def __init__(self,
-                 adj_list: Dict[int, List[int]],
-                 weights: Optional[Dict[Tuple[int, int], int]] = None):
+                 adj_list: Dict[int, List[int]]):
         self.adj_list = adj_list
-        self.weights = dict() if weights is None else weights
         self.max_vertex = -1
         for v, ends in self.adj_list.items():
             self.max_vertex = max([self.max_vertex, v] + ends)
 
     @staticmethod
-    def from_file(input_path: Path, weighted: bool = False) -> "Graph":
-        if weighted:
-            return Graph._from_file_weighted(input_path)
+    def from_file(input_path: Path, sep: str = " ") -> "Graph":
         adj_list = defaultdict(list)
         with input_path.open("r") as input_file:
             for line in input_file.readlines():
-                items = list(map(int, line.strip().split(" ")))
+                items = list(map(int, line.strip().split(sep)))
                 adj_list[items[0]] += items[1:]
         return Graph(adj_list)
-
-    @staticmethod
-    def _from_file_weighted(input_path: Path) -> "Graph":
-        adj_list = defaultdict(list)
-        weights = dict()
-        with input_path.open("r") as input_file:
-            for line in input_file.readlines():
-                items = line.strip().split(" ")
-                source = int(line[0])
-                edges = [tuple(map(int, edge.strip().split(",")))
-                         for edge in items[1:]]
-                adj_list[source] = [edge[0] for edge in edges]
-                for end, weight in edges:
-                    weights[(source, end)] = weight
-        return Graph(adj_list, weights)
 
     def reverse(self) -> "Graph":
         adj_list = defaultdict(list)
@@ -47,3 +29,24 @@ class Graph:
             for end in ends:
                 adj_list[end].append(vertex)
         return Graph(adj_list)
+
+
+class WeightedGraph:
+    def __init__(self, adj_list: Dict[int, List[Tuple[int, int]]]):
+        self.adj_list = adj_list
+        self.max_vertex = -1
+        for v, ends in self.adj_list.items():
+            self.max_vertex = max([self.max_vertex, v] +
+                                  [x[1] for x in ends])
+
+    @staticmethod
+    def from_file(input_path: Path, sep: str = " ") -> "WeightedGraph":
+        adj_list = defaultdict(list)
+        with input_path.open("r") as input_file:
+            for line in input_file.readlines():
+                items = line.strip().split(sep)
+                source = int(line[0])
+                adj_list[source] = [tuple(map(int, edge.strip().split(",")))
+                                    for edge in items[1:]]
+
+        return WeightedGraph(adj_list)
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from vvalgo.graph import Graph
+from vvalgo.graph import Graph, WeightedGraph
 
 
 @pytest.fixture()
@@ -31,8 +31,8 @@ def test_graph_reverse(graph_adj_list):
     """Test reverse of graph."""
     graph = Graph(adj_list=graph_adj_list)
     reversed_graph = graph.reverse()
-    assert reversed_graph.adj_list == {0: {2}, 1: {0, 2, 3}, 2: {0, 1},
-                                       3: {0, 1}}
+    assert reversed_graph.adj_list == {0: [2], 1: [0, 2, 3], 2: [0, 1],
+                                       3: [0, 1]}
     assert reversed_graph.max_vertex == 3
 
 
@@ -53,8 +53,8 @@ def graph_file_weighted(tmpdir, graph_adj_list):
 
 
 def test_graph_from_file_weighted(graph_file_weighted, graph_adj_list):
-    graph = Graph.from_file(graph_file_weighted, weighted=True)
-    assert graph.adj_list == graph_adj_list
-    assert graph.max_vertex == 3
-    assert graph.weights == {(0, 1): 1, (0, 2): 1, (0, 3): 1, (1, 2): 2,
-                             (1, 3): 2, (2, 0): 3, (2, 1): 3, (3, 1): 4}
+    graph = WeightedGraph.from_file(graph_file_weighted)
+    assert graph.max_vertex == 4
+    assert graph.adj_list == {k: list(map(lambda x: (x, i), values))
+                              for i, (k, values) in
+                              enumerate(graph_adj_list.items(), 1)}
